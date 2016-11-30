543657fe6a766d44e765d2cd829498e167e40e38
aorfanos1 <alex.orfanos13@gmail.com>
2016-11-30 14:55:35 -0500

    Testing example #1.

---
+++ mean_testing.R
@@ -0,0 +1,37 @@
+####A/B Testing Class 11/30
+
+set.seed(3)
+grp.1 <- round(rlnorm(100,6))
+grp.2 <- round(rlnorm(100,6))
+
+hist(grp.1)
+hist(grp.2)
+data <- c(grp.1, grp.2)
+
+l1 <- length(grp.1)
+l2 <- length(grp.2)
+lt <- l1 + l2
+test.diff <- mean(grp.1) - mean(grp.2)
+
+it <- function(n){
+  M = NULL
+  for (i in 1:n){
+    s = sample(data, lt, FALSE)
+    m1 = mean(s[1:l1]) - mean(s[(l1+1):lt])
+    M = c(M, m1)
+  }
+  return(M)
+}
+
+examples <- it(10000)
+
+par(mfrow = c(1,1))
+hist(examples, col = 'red', breaks = 100)
+abline(v = test.diff, col = 'black', lwd = 4)
+
+#1 tail test
+(sum(examples<test.diff))/10000
+(sum(examples>test.diff))/10000
+
+
+
